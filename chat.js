69c88c3834e9e2a07e3062fe2f8647b6ea136cfa
yurigle <connect1class@gmail.com>
2020-06-02 03:35:50 +0900

    main js

---
+++ chat.js
@@ -0,0 +1,10 @@
+window.onload = function()
+{
+    var view = document.getElementById("chat_wrap"); 
+    var window_height = window.innerHeight; // 브라우저 화면 높이값
+    console.log(window_height);
+    view.style.setProperty('height',window_height+'px'); // 적용
+
+    
+}
+
